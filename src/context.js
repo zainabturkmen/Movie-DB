cc17067b66339bc89cf7bc13264159c8914362c0
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-16 11:15:00 +0430

    MovieDB

--- src/context.js
+++ src/context.js
@@ -8,7 +8,7 @@ const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState({ show: false, msg: "" });
   const [movie, setMovie] = useState([]);
-
+  const [quer, setQuery] = useState("batman")
   return <AppContext.Provider value="heloo">{children}</AppContext.Provider>;
 };
 
