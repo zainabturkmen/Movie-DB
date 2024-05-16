3d910e6aebc8cf65a063389a6ee645945d21fafc
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-16 09:41:13 +0430

    MovieDB

--- src/context.js
+++ src/context.js
@@ -1,17 +1,17 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from "react";
 // make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 
 const AppContext = React.createContext();
 
-const AppProvider = ({children}) => {
-  return <AppContext.Provider value="heloo">{children}</AppContext.Provider>
-}
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="heloo">{children}</AppContext.Provider>;
+};
 
 // make sure use
 
 export const useGlobalContext = () => {
-  return useContext(AppContext)
+  return useContext(AppContext);
 };
 
-export {AppContext, AppProvider}
+export { AppContext, AppProvider };
