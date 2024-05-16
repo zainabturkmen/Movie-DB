4f9b14934055c48a2e27befdfe4faf30ac4fb164
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-16 16:20:47 +0430

    MovieDB

--- src/context.js
+++ src/context.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 // make sure to use https
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+console.log(API_ENDPOINT);
 
 const AppContext = React.createContext();
 
